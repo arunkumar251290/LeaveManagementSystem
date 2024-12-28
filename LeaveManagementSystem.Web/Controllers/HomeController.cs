432bfb9a734f4bc6fde736878f1882fc8999a401
DELL <DELL@DESKTOP-93AN3DE>
2024-12-28 11:33:45 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var model = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(model);
+
+        }
+    
+    }
+}
